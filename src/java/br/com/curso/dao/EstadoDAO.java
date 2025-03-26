ceb2d747f3a537a92e71f76f03c067995f7e73d6
Edsincp <edsonpereiracarvalho10@gmail.com>
2025-03-26 08:35:15 -0300

    commit inicial

---
+++ src/java/br/com/curso/dao/EstadoDAO.java
@@ -0,0 +1,57 @@
+
+package br.com.curso.dao;
+
+import br.com.curso.utils.SingleConnection;
+import java.sql.Connection;
+import java.util.List;
+
+public class EstadoDAO implements GenericDAO {
+    
+    private Connection conexao;
+    
+    public EstadoDAO() throws Exception{
+        conexao = SingleConnection.getConnection();
+    }
+
+    @Override
+    public Boolean cadastrar(Object Objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Boolean inserir(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Boolean alterar(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Boolean excluir(int numero) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object carregar(int numero) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Object> listar() {
+         List<Object> resultado = new ArrayList<>();
+         PreparedStatement stmt = null;
+         ResultSet rs = null;
+         String sql = "Select * from estado order by idEstado";
+         try {
+             stmt = conexao.prepareStatement(sql);
+             rs = stmt.executeQuery();
+             while (rs.next()) {
+                 Estado oEstado = new Estado();
+                 oEstado.settIdEstado
+             }
+         }
+    }
+    
+}
---
+++ web/header.jsp
@@ -0,0 +1,20 @@
+<%@page contentType="text/html" pageEncoding="iso-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-Type" content="text/html; charset=iso-8859-1">
+<title>JPS Page</title>
+<script src="${pageContext.request.contextPath}/js/jquery-3.3.1.min.js"></script>
+<script src="${pageContext.request.contextPath}/js/jquery.mask.min.js"></script>
+<script src="${pageContext.request.contextPath}/js/jquery.maskMoney.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://dcn.datatables.net/1.10.22/css/jquery.dataTables.min.css"/>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js" type="text/javascript"></script>
+
+ 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.3.1/dist/sweetalert2.all.min.js" type="text/javascript"></script>
+</head>
+<body>
